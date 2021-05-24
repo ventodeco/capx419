d372a1ad4822c30a4642c6ddab62eb4fb440e280
Intern-Ibrahim <mubarokibrahim@gmail.com>
2021-05-24 22:02:07 +0700

    (implement-camera) : Add feature image picker (open camera and open gallery)

--- android-app/food-facts/FoodFactsApp/app/src/main/java/com/capx419/foodfactsapp/presentation/openimage/OpenImageActivity.kt
+++ android-app/food-facts/FoodFactsApp/app/src/main/java/com/capx419/foodfactsapp/presentation/openimage/OpenImageActivity.kt
@@ -1,12 +1,16 @@
 package com.capx419.foodfactsapp.presentation.openimage
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.capx419.foodfactsapp.databinding.ActivityOpenImageBinding
 import com.capx419.foodfactsapp.utils.emptyString
+import com.esafirm.imagepicker.features.ImagePicker
+import com.esafirm.imagepicker.features.IpCons
 
 class OpenImageActivity : AppCompatActivity() {
 
@@ -18,6 +22,8 @@ class OpenImageActivity : AppCompatActivity() {
     }
 
     private lateinit var openImageBinding: ActivityOpenImageBinding
+
+    private var image = emptyString()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,12 +37,32 @@ class OpenImageActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         openImageBinding.btnCamera.setOnClickListener {
-
+            openCamera()
         }
 
         openImageBinding.btnGallery.setOnClickListener {
-
+            openGallery()
         }
+    }
+
+    private fun openGallery() {
+        ImagePicker.create(this)
+            .single()
+            .folderMode(true)
+            .start()
+    }
+
+    private fun openCamera() {
+        ImagePicker.cameraOnly().start(this)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode == Activity.RESULT_OK && requestCode == IpCons.RC_IMAGE_PICKER && data != null) {
+            image = ImagePicker.getImages(data)[0].path
+            Toast.makeText(this, image, Toast.LENGTH_LONG).show()
+            return
+        }
+        super.onActivityResult(requestCode, resultCode, data)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
--- android-app/food-facts/FoodFactsApp/app/src/main/java/com/capx419/foodfactsapp/utils/Const.kt
+++ android-app/food-facts/FoodFactsApp/app/src/main/java/com/capx419/foodfactsapp/utils/Const.kt
@@ -3,4 +3,6 @@ package com.capx419.foodfactsapp.utils
 object Const {
     const val EXTRA_STRING = "extra_string"
     const val CUSTOM_FRAGMENT = "custom_fragment"
+    const val IMG_GALLERY_REQUEST_CODE = 91
+    const val IMG_CAMERA_REQUEST_CODE = 101
 }
