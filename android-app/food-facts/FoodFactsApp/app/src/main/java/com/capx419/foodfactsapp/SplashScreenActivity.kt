42c3d0d53692195c7e33a335a92d058d76bfe8f7
ocid <arnarasyid79@gmail.com>
2021-05-24 15:34:48 +0700

    splash_screen

--- android-app/food-facts/FoodFactsApp/app/src/main/java/com/capx419/foodfactsapp/SplashScreenActivity.kt
+++ android-app/food-facts/FoodFactsApp/app/src/main/java/com/capx419/foodfactsapp/SplashScreenActivity.kt
@@ -4,15 +4,19 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+
+import android.os.Looper
 import android.view.WindowManager
 
-class SplashScreen : AppCompatActivity() {
+class SplashScreenActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
-        Handler().postDelayed({
-            val intent : Intent(this, MainActivity::class.java)
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
         },3000)
